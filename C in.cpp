77510304a12a835602da978074d7439116149532
Rehman ch <124032207+Rehmanch788@users.noreply.github.com>
2023-02-18 22:14:45 -0800

    Add files via upload
    Basics for input and output in c++ Language

---
+++ C in.cpp
@@ -0,0 +1,11 @@
+
+#include <iostream>  //Preprocessive directive
+using namespace std;  //Library function
+int main( ) //main function
+{  
+  int Age;  
+   cout << "Enter your Age=";   
+   cin >> Age;    //save the age in declare variabe
+   cout << "Your Age is=" << Age << "\n"; 
+   return 0; //end the function
+}   
---
+++ C out.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+cout<<"Rehman Atta"<<"\n";
+  cout <<"I am learning C++ from university of Education Lahore,Multan Campus";
+  return 0;
+}
